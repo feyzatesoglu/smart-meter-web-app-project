d39ffc314a486fd0a72346a4bbff53047f04ae1e
feyzatesoglu <atesogluf35@gmail.com>
2023-11-08 21:32:35 +0300

    Codes are added.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LogInComponent } from './User/log-in/log-in.component';
+import { SignUpComponent } from './User/sign-up/sign-up.component';
+import { HomeComponent } from './HomePage/home/home.component';
+
+const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'log-in', component: LogInComponent},
+  {path:'sign-up', component: SignUpComponent},
+  {path:'',redirectTo:'home',pathMatch:'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LogInComponent } from './User/log-in/log-in.component';
+import { SignUpComponent } from './User/sign-up/sign-up.component';
+import { HomeComponent } from './HomePage/home/home.component';
+import { NavMenuComponent } from './NavMenu/nav-menu/nav-menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LogInComponent,
+    SignUpComponent,
+    HomeComponent,
+    NavMenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
