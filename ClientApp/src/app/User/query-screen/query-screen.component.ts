a127145e8b151f4b411b906b1edca2922a4b0935
feyzatesoglu <atesogluf35@gmail.com>
2023-11-20 20:15:45 +0300

    Yeni componentler eklendi, düzenlemeler yapıldı.

--- ClientApp/src/app/User/query-screen/query-screen.component.ts
+++ ClientApp/src/app/User/query-screen/query-screen.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-query-screen',
   templateUrl: './query-screen.component.html',
   styleUrls: ['./query-screen.component.css']
 })
-export class QueryScreenComponent {
+export class QueryScreenComponent implements OnInit {
+  form!: FormGroup;
 
+  question1answers: string [] = ['Zorlu', 'Basit'];
+  question2answers: string [] = ['Kentsel', 'Kırsal'];
+  question3answers: string [] = ['Zorlu', 'Basit'];
+  question4answers: string [] = ['Sürekli', 'Aralıklı'];
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      // Form kontrol tanımları burada olacak
+      // Örnek olarak:
+      question1: ['', Validators.required],
+      question2: ['', Validators.required],
+      question3: ['', Validators.required],
+      question4: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    // Form gönderildiğinde yapılacak işlemler
+    if (this.form.valid) {
+      // Form doğrulandı, verileri gönderebilirsiniz
+      console.log(this.form.value);
+    } else {
+      // Form geçerli değil, gerekli alanları doldurun uyarısı verebilirsiniz
+      // Örn: this.form.controls['question1'].markAsTouched();
+    }
+  }
 }
--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { LogInComponent } from './User/log-in/log-in.component';
 import { SignUpComponent } from './User/sign-up/sign-up.component';
 import { HomeComponent } from './HomePage/home/home.component';
+import { QueryScreenComponent } from './User/query-screen/query-screen.component';
+import { ContactComponent } from './User/contact/contact.component';
 
 const routes: Routes = [
   {path:'anasayfa', component: HomeComponent},
   {path:'giris-yap', component: LogInComponent},
   {path:'uye-ol', component: SignUpComponent},
-  {path:'',redirectTo:'home',pathMatch:'full'}
+  {path:'sorgula', component: QueryScreenComponent},
+  {path:'anasayfa',redirectTo:'home',pathMatch:'full'},
+  {path:'iletisim', component:ContactComponent}
 ];
 
 @NgModule({
