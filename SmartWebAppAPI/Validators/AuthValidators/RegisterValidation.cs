9cd648e0c66c87ce5e2046d08717612c528de9d2
zeynepdanis <you@example.com>
2024-01-23 00:02:41 +0300

    Update password validation rules

--- SmartWebAppAPI/Validators/AuthValidators/RegisterValidation.cs
+++ SmartWebAppAPI/Validators/AuthValidators/RegisterValidation.cs
@@ -22,11 +22,13 @@ namespace SmartWebAppAPI.Validators.AuthValidators
           .EmailAddress().WithMessage("Invalid email address");
 
       RuleFor(x => x.Password)
-          .NotEmpty().WithMessage("Password cannot be empty")
-          .MinimumLength(6).WithMessage("Password must be at least 6 characters")
-          .Matches("[A-Z]").WithMessage("Password must contain at least one uppercase letter")
-          .Matches("[a-z]").WithMessage("Password must contain at least one lowercase letter")
-          .Matches("[/?=+!@#$%^&*]").WithMessage("Password must contain at least one special character");
+    .NotEmpty().WithMessage("Password cannot be empty")
+    .MinimumLength(6).WithMessage("Password must be at least 6 characters")
+    .Matches("[A-Z]").WithMessage("Password must contain at least one uppercase letter")
+    .Matches("[a-z]").WithMessage("Password must contain at least one lowercase letter")
+    .Matches("[0-9]").WithMessage("Password must contain at least one numeric digit")
+    .Matches("[^a-zA-Z0-9]").WithMessage("Password must contain at least one special character");
+
 
     }
   }
